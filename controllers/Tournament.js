bf5ba6e7c5edfa74f488826faa56617e6b5dfddd
Robert Banaszak <robert.banaszak1997@gmail.com>
2018-12-31 23:12:20 +0100

    get tournament

--- controllers/Tournament.js
+++ controllers/Tournament.js
@@ -1,5 +1,10 @@
 const Tournament = require('../models/Tournament');
+const SkiJumpingHill = require('../models/SkiJumpingHill');
+const IndividualCompetition = require('../models/IndividualCompetition');
+const TeamCompetition = require('../models/TeamCompetition');
+const Competition = require('../models/Competition');
 const { Op } = require('sequelize');
+const { deletePrefixes } = require('../utils/deletePrefixes');
 
 const relationData = {
   create: {
@@ -31,8 +36,78 @@ exports.createTournament = async (req, res) => {
   create(req, res, relationData);
 };
 
-exports.getTournament = async (req, res) => {
+exports.getTournaments = async (req, res) => {
   get(req, res, relationData);
+};
+
+exports.getTournament = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const skiJumpingHills = await SkiJumpingHill.findAll({ raw: true });
+    // get individual competition and it's ski jumping hills
+    const individualCompetitions = await IndividualCompetition.findAll({
+      include: [
+        {
+          model: Competition,
+          where: { tournament_id: id },
+          raw: true,
+          required: true,
+          nested: false
+        }
+      ],
+      raw: true
+    });
+    const parsedIndividualCompetition = deletePrefixes(individualCompetitions);
+    const indWithHills = parsedIndividualCompetition.map(comp => {
+      const hill = skiJumpingHills.filter(
+        hill => hill.ski_jumping_hill_id === comp.ski_jumping_hill_id
+      )[0];
+      return {
+        ...hill,
+        ...comp
+      };
+    });
+    // get team competition and it's ski jumping hills
+    const teamCompetitions = await TeamCompetition.findAll({
+      include: [
+        {
+          model: Competition,
+          where: { tournament_id: id },
+          raw: true,
+          required: true,
+          nested: false
+        }
+      ],
+      raw: true
+    });
+    const parsedTeamCompetition = deletePrefixes(teamCompetitions);
+    const teamWithHills = parsedTeamCompetition.map(comp => {
+      const hill = skiJumpingHills.filter(
+        hill => hill.ski_jumping_hill_id === comp.ski_jumping_hill_id
+      )[0];
+      return {
+        ...hill,
+        ...comp
+      };
+    });
+    // get tournaments
+    const tournament = await Tournament.findById(id, { raw: true });
+    res.status(200).json({
+      status: 'success',
+      tournament: {
+        ...tournament,
+        individualCompetitions: indWithHills,
+        teamCompetitions: teamWithHills
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 'failure',
+      error
+    });
+  }
 };
 
 exports.updateTournament = async (req, res) => {
--- routes/Tournament.js
+++ routes/Tournament.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const TournamentController = require('../controllers/tournament');
 router.post('/', TournamentController.createTournament);
-router.get('/', TournamentController.getTournament);
+router.get('/', TournamentController.getTournaments);
+router.get('/:id', TournamentController.getTournament);
 router.patch('/', TournamentController.updateTournament);
 router.delete('/', TournamentController.deleteTournament);
 
